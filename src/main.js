ca6fa12b303268ac12ee791caf6cdcc33dcd0000
Andry_Koss <pokemon.322.go@gmail.com>
2020-09-17 12:13:07 +0300

    установленн firebase, сделана авторизация

--- src/main.js
+++ src/main.js
@@ -4,16 +4,40 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import dateFilter from "./filters/date.filter";
+import messagePlugin from './utils/message.plugin'
 import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min'
 
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+
 Vue.config.productionTip = false
 
+Vue.use(messagePlugin)
 Vue.use(Vuelidate)
 Vue.filter('date', dateFilter)
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+firebase.initializeApp({
+  apiKey: "AIzaSyDzM4xoWN-vsIHhTjU21Z2KWBNc0BxqrNM",
+  authDomain: "homebookkeeping-bbd22.firebaseapp.com",
+  databaseURL: "https://homebookkeeping-bbd22.firebaseio.com",
+  projectId: "homebookkeeping-bbd22",
+  storageBucket: "homebookkeeping-bbd22.appspot.com",
+  messagingSenderId: "995195030876",
+  appId: "1:995195030876:web:47619183d655c5de10c599"
+});
+
+let app;
+
+firebase.auth().onAuthStateChanged(() => {
+  if(!app){
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
+
+
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -60,6 +60,7 @@
 
 <script>
     import {email, required, minLength} from 'vuelidate/lib/validators'
+    import messages from "../utils/messages";
 
     export default {
         name: 'login',
@@ -71,8 +72,13 @@
             email: {email, required},
             password: {required, minLength: minLength(8)}
         },
+        mounted() {
+            if(messages[this.$route.query.message]){
+                this.$message(messages[this.$route.query.message])
+            }
+        },
         methods: {
-            submitHandler(){
+            async submitHandler(){
                 if(this.$v.$invalid){
                     this.$v.$touch()
                     return
@@ -82,8 +88,12 @@
                     password: this.password
                 }
 
-                console.log(formData)
-                this.$router.push('/')
+                try{
+                    await this.$store.dispatch('login', formData);
+                    this.$router.push('/');
+                }
+                catch (e) { }
+
             }
         }
     }
