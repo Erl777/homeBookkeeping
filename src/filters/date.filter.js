831b6471a79606b64a762012168f5462354805d5
Andry_Koss <pokemon.322.go@gmail.com>
2020-10-14 11:06:52 +0300

    Урок 19. Динамическая локализация (ru/en)

--- src/filters/date.filter.js
+++ src/filters/date.filter.js
@@ -1,3 +1,5 @@
+import store from '../store'
+
 export  default function dateFilter(value, formate = 'date') {
     // console.log(value);
     const options = {};
@@ -14,5 +16,6 @@ export  default function dateFilter(value, formate = 'date') {
         options.second = '2-digit'
     }
 
-    return new Intl.DateTimeFormat('ru-RU', options).format(new Date(value))
+    const locale = store.getters.info.locale || 'ru-RU'
+    return new Intl.DateTimeFormat(locale, options).format(new Date(value))
 }
